38c05b069e1acdad2dc101b6c86b3225a3a1fd26
Sarmaon <patratiyaa@gmail.com>
2022-12-16 17:57:48 +0800

    Result Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, 
+// а какое меньшее.
+
+Console.WriteLine("Введите первое число ");
+ int number1 = Convert.ToInt32(Console.ReadLine());
+
+ Console.WriteLine("Введите второе число ");
+ int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+Console.WriteLine("Первое число больше");
+else Console.WriteLine("Второе число больше");
