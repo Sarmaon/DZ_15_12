088e6db3c2afbe300401c6ffd89b658cfa384459
Sarmaon <patratiyaa@gmail.com>
2022-12-17 11:20:10 +0800

    Result Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,12 +2,17 @@
 // и выдаёт максимальное из этих чисел.
 
 Console.WriteLine("Введите первое число ");
- int number1 = Convert.ToInt32(Console.ReadLine());
+ int max = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число ");
- int number2 = Convert.ToInt32(Console.ReadLine());
+ int number1 = Convert.ToInt32(Console.ReadLine());
 
  Console.WriteLine("Введите третье число ");
- int number3 = Convert.ToInt32(Console.ReadLine());
+ int number2 = Convert.ToInt32(Console.ReadLine());
 
- 
+ if (max < number1)
+ max = number1;
+
+ else if (max < number2)
+ max = number2;
+Console.WriteLine($"Максимальное число {max}");
